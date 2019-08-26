0b9b12d7296a1f59955a0bd8b739a23d933a23e1
coti_garcia <javiera.garcialatorre@gmail.com>
2019-08-26 18:34:27 -0400

    API routes created

---
+++ routes/api-routes.js
@@ -0,0 +1,9 @@
+const db = require("./models");
+
+module.exports = function(app){
+    app.get("/api/products"), function(req,res){
+        db.Product.findAll({}).then(function (dbProduct){
+            res.json(dbProduct)
+        })
+    }
+}
