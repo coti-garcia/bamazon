f804300f0eee247a48c7ebc82c593968bf951fac
coti_garcia <javiera.garcialatorre@gmail.com>
2019-08-27 22:56:14 -0400

    trying to update database

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,5 @@
 const db = require("../models");
-
+let cart = []
 module.exports = function(app){
     app.get("/api/products", function(req,res){
         db.Product.findAll({}).then(function (dbProduct){
@@ -9,17 +9,35 @@ module.exports = function(app){
     app.post("/api/products", function (req, res) {
         let idProduct = req.body.id
         let qty = req.body.qty
-        
         db.Product.findOne({
             where: {
                 id: idProduct
             }
         }).then(function(product){
             if( product.stock_quantity > qty){
-                res.json(product);
+                product.qty = qty;
+                cart.push(product) 
+                
+                // Update Stock in DB
+                let newStock = product.stock_quantity - qty;
+                app.put("/api/products", function(req, res) {
+                    console.log("New Stock:" + newStock)
+                    db.Product.update({
+                        stock_quantity: newStock,
+                        where: {
+                            id: idProduct
+                        }
+                      }).then(function(dbProducts) {
+                            res.json(dbProducts);
+                    });
+                });
+
+                res.json(cart);
+
             }else{
                 res.send("The stock is not enough");
             }
         });
     });
+
 }
